30893dac7caaf3b0dd148593b2486e81b3db94df
iuryr <iuryroberto@gmail.com>
2025-05-12 16:16:07 -0300

    lock mason.nvim and mason-lspconfig.nvim versions

--- init.lua
+++ init.lua
@@ -98,8 +98,8 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
-      'williamboman/mason-lspconfig.nvim',
+      { 'williamboman/mason.nvim', version = "v1.11.0", config = true },
+		{'williamboman/mason-lspconfig.nvim', version = "v1.32.0"},
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
